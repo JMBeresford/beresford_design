cf0639d1090ed1a3c9ede2643c416b99d6eb7e16
John Beresford <jberesford@volcaus.com>
2020-12-31 19:19:25 -0800

    netlify form test

--- script/main.js
+++ script/main.js
@@ -4,6 +4,7 @@ const contact_btn = document.querySelector('#contact-us');
 const brightness_mode = document.getElementById('dark-mode');
 const modal_bg = document.getElementById('contact-wrapper');
 const modal = document.getElementById('contact');
+const send_btn = document.getElementById('contact-btn');
 
 function addContactAnimation() {
   document.querySelector('#contact-wrapper').classList.add("animate");
@@ -15,8 +16,15 @@ function removeContactAnimation() {
   document.querySelector('#contact').classList.remove("animate");
 }
 
+function sendContact() {
+  document.contact.submit();
+  document.querySelector('#contact-wrapper').classList.remove("animate");
+  document.querySelector('#contact').classList.remove("animate");
+}
+
 close_btn.addEventListener('click', removeContactAnimation);
 modal_bg.addEventListener('click', removeContactAnimation);
+send_btn.addEventListener('click', sendContact);
 contact_btn.addEventListener('click', addContactAnimation);
 find_yours_btn.addEventListener('click', addContactAnimation);
 
