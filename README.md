b71ad5fc4d1d59953b0c6e75cc96dee35e58f5a2
John Beresford <jberesford@volcaus.com>
2021-08-04 14:23:35 -0700

    redesign

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# beresford_design
-Web Page for my (tentative) design company
-
-## Now hosted [here](https://beresforddesign.net)
-
-Soon to be refactored using [Gatsby.js](https://github.com/gatsbyjs/gatsby)
+# Under Construction
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import '../styles/main.scss';
+
+// markup
+const IndexPage = () => {
+  return (
+    <main id='hero'>
+      <h1>
+        Stand out from the competition with a bespoke digital masterpiece.
+      </h1>
+    </main>
+  );
+};
+
+export default IndexPage;
