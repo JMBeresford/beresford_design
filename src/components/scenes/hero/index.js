74900183019b7dccba39840c6ad7c91e232e7ba7
John Beresford <jberesford@volcaus.com>
2021-09-03 21:11:16 -0700

    build bugfix

--- src/components/scenes/hero/index.js
+++ src/components/scenes/hero/index.js
@@ -5,10 +5,13 @@ import { Box } from '@react-three/drei';
 import Model from './Beresford_design';
 
 const Hero = () => {
+  const pixelRatio =
+    typeof window !== 'undefined' ? window.devicePixelRatio : 1;
+
   return (
     <Canvas
       id='heroCanvas'
-      dpr={Math.min(window.devicePixelRatio, 2)}
+      dpr={Math.min(pixelRatio, 2)}
       camera={{ position: [7.815515, 8.291183, 10.41677] }}
       gl={{ alpha: false, antialias: true }}
       onCreated={({ gl }) => gl.setClearAlpha(0xf1f6f9)}
