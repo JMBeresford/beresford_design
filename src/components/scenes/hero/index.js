69d74342de276a6d402260d20cb2f1f1c484a418
John Beresford <jberesford@volcaus.com>
2021-09-06 22:56:49 -0700

    scene assets finalized

--- src/components/scenes/hero/index.js
+++ src/components/scenes/hero/index.js
@@ -1,75 +1,15 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Model from './Beresford_design';
-import { graphql, useStaticQuery } from 'gatsby';
 
-const Hero = () => {
+const Hero = (props) => {
   const pixelRatio =
     typeof window !== 'undefined' ? window.devicePixelRatio : 1;
 
-  const query = useStaticQuery(graphql`
-    query {
-      bake2: file(relativePath: { eq: "bake2.jpg" }) {
-        childImageSharp {
-          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
-            src
-          }
-        }
-      }
-      bake3: file(relativePath: { eq: "bake3.jpg" }) {
-        childImageSharp {
-          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
-            src
-          }
-        }
-      }
-      bake4: file(relativePath: { eq: "bake4.jpg" }) {
-        childImageSharp {
-          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
-            src
-          }
-        }
-      }
-      bake5: file(relativePath: { eq: "bake5.jpg" }) {
-        childImageSharp {
-          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
-            src
-          }
-        }
-      }
-      bake6: file(relativePath: { eq: "bake6.jpg" }) {
-        childImageSharp {
-          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
-            src
-          }
-        }
-      }
-      bake7: file(relativePath: { eq: "bake7.jpg" }) {
-        childImageSharp {
-          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
-            src
-          }
-        }
-      }
-      bake8: file(relativePath: { eq: "bake8.jpg" }) {
-        childImageSharp {
-          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
-            src
-          }
-        }
-      }
-    }
-  `);
-
   return (
-    <Canvas
-      id='heroCanvas'
-      dpr={Math.min(pixelRatio, 2)}
-      camera={{ position: [7.815515, 8.291183, 10.41677] }}
-      gl={{ alpha: false, antialias: true }}
-    >
+    <Canvas id='heroCanvas' dpr={Math.min(pixelRatio, 2)} gl={{ alpha: false }}>
       <Suspense fallback={null}>
-        <Model data={query} />
+        <Model data={props.query} />
       </Suspense>
     </Canvas>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,68 @@
 import * as React from 'react';
 import '../styles/main.scss';
 import Hero from '../components/scenes/hero';
+import { graphql, useStaticQuery } from 'gatsby';
 
 // markup
 const IndexPage = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      bake2: file(relativePath: { eq: "bakes/bake2.png" }) {
+        childImageSharp {
+          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
+            src
+          }
+        }
+      }
+      bake3: file(relativePath: { eq: "bakes/bake3.png" }) {
+        childImageSharp {
+          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
+            src
+          }
+        }
+      }
+      bake4: file(relativePath: { eq: "bakes/bake4.png" }) {
+        childImageSharp {
+          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
+            src
+          }
+        }
+      }
+      bake5: file(relativePath: { eq: "bakes/bake5.png" }) {
+        childImageSharp {
+          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
+            src
+          }
+        }
+      }
+      bake6: file(relativePath: { eq: "bakes/bake6.png" }) {
+        childImageSharp {
+          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
+            src
+          }
+        }
+      }
+      bake7: file(relativePath: { eq: "bakes/bake7.png" }) {
+        childImageSharp {
+          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
+            src
+          }
+        }
+      }
+      bake8: file(relativePath: { eq: "bakes/bake8.png" }) {
+        childImageSharp {
+          fixed(height: 2048, width: 2048, quality: 100, jpegQuality: 100) {
+            src
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <div id='index'>
       <main id='hero'>
-        <Hero />
+        <Hero query={query} />
       </main>
     </div>
   );
