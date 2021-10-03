fcacbd3335896e148ff03c99b9b4da133a0781dc
John Beresford <jberesford@volcaus.com>
2021-10-03 15:16:54 -0700

    :sparkles: init rewrite

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Loading from './components/loading/Loading';
+
+function App() {
+  return (
+    <div className='App'>
+      <Loading />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/loading/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div id='loading'>
+      <div className='imgWrapper'>
+        <img className='logo' src={logo} alt='Beresford Design Logo' />
+      </div>
+
+      <h2>LOADING</h2>
+    </div>
+  );
+};
+
+export default Loading;
