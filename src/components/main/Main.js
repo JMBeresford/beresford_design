00f8bf1194c859aa1a8ffc825073e9a1f8cac1c9
John Beresford <jberesford@volcaus.com>
2021-10-10 16:58:04 -0700

    etc

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import HeroScene from './HeroScene';
 import Header from './Header';
 import useStore from '../../store';
@@ -7,9 +7,7 @@ const Main = () => {
   return (
     <div id='landing'>
       <Header />
-      <Suspense fallback={null}>
-        <HeroScene />
-      </Suspense>
+      <HeroScene />
     </div>
   );
 };
